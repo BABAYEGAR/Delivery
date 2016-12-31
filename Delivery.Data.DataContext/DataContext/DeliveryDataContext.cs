372554723395e96f477f1eacacf0281a1274bcaf
Haruna Salim <salxsaa@gmail.com>
2016-12-31 21:18:16 +0100

    Added the delivery data context and transport object

---
+++ Delivery.Data.DataContext/DataContext/DeliveryDataContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delivery.Data.DataContext.DataContext
+{
+    public class DeliveryDataContext : DbContext
+    {
+        public DeliveryDataContext() : base("Delivery")
+        {
+            
+        }
+        
+    }
+}
